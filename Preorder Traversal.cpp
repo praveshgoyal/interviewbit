ec8745fdc41ba242b68fd88966e8bf5f293608eb
Pravesh Goyal <praveshgoyal26f97@gmail.com>
2019-04-23 01:23:12 +0530

    Create Preorder Traversal.cpp

---
+++ Preorder Traversal.cpp
@@ -0,0 +1,51 @@
+
+
+Given a binary tree, return the preorder traversal of its nodes’ values.
+
+Example :
+Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Using recursion is not allowed.
+
+
+
+////cpp
+
+
+
+
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ vector<int> v ;
+void func(TreeNode* A){
+    if(A==NULL)
+        return ;
+    v.push_back(A->val) ;
+    func(A->left) ;
+    func(A->right) ;
+    
+}
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    v.clear() ;
+    func(A) ;
+    return v ;
+}
